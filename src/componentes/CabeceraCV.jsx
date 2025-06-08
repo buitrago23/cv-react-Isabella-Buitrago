5bb8b7f3fb1412cf31f2a508d6633e5cfcc5a4ef
isabella <gallegobuitragoisa@gmail.com>
2025-06-08 09:36:30 -0500

    Agrega archivos iniciales del proyecto CV en React

---
+++ src/componentes/CabeceraCV.jsx
@@ -0,0 +1,8 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Isabella Buitrago Galeego</h1>
+      <h2>Desarrolladora Full Stack y cloud enginner</h2>
+    </header>
+  );
+}
---
+++ src/componentes/Perfil.jsx
@@ -0,0 +1,12 @@
+export default function Perfil() {
+  return (
+    <section>
+      <h3>Perfil</h3>
+      <p>
+        Soy una desarrolladora full stack con conocimientos en frontend, backend y cloud. Me apasiona crear aplicaciones funcionales de principio a fin: desde el diseño de interfaces intuitivas hasta la lógica del servidor y su despliegue en la nube. He trabajado con tecnologías como HTML, CSS, JavaScript, React, Node.js y bases de datos como MongoDB.
+También tengo experiencia usando servicios en la nube como AWS y herramientas como Docker, lo que me permite desarrollar soluciones escalables y bien estructuradas.
+Me gusta aprender constantemente, enfrentar nuevos retos y trabajar en equipo para lograr productos de calidad que realmente aporten valor. Siempre busco mejorar mis habilidades y aportar lo mejor de mí en cada proyecto.
+      </p>
+    </section>
+  );
+}
